93cd3ac2a289f4f95afca8c2a9f73704985af215
George Davies <georgedavies@iMac.local>
2017-08-25 14:04:39 +0100

    get weather data setup

---
+++ WeatherApp/WeatherDataManager.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherDataManager.swift
+//  WeatherApp
+//
+//  Created by George Davies on 25/08/2017.
+//  Copyright © 2017 George Davies. All rights reserved.
+//
+
+import Foundation
+
+class WeatherData: NSObject {
+    
+}
