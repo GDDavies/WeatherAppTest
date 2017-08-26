2cbc5351d524fabbd87d9d4edaad011b71a408ad
George <gdaviesdev@gmail.com>
2017-08-26 01:07:06 +0100

    Map search zooms to area

--- WeatherApp/MapVC.swift
+++ WeatherApp/MapVC.swift
@@ -31,17 +31,15 @@ class MapVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let userLocation = location {
-            let span = MKCoordinateSpanMake(0.05, 0.05)
-            let region = MKCoordinateRegion(center: userLocation.coordinate, span: span)
-            mapView.setRegion(region, animated: true)
-        }
         let searchLocationButton = UIBarButtonItem(
             title: "Weather Search",
             style: .plain,
             target: self,
             action: #selector(searchLocationAction(sender:))
         )
+        if let startLocation = location {
+            goToLocationOnMap(location: startLocation)
+        }
         
         self.navigationItem.setRightBarButton(searchLocationButton, animated: true)
         self.navigationController?.title = ""
@@ -50,6 +48,18 @@ class MapVC: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func goToLocationOnMap(location: CLLocation) {
+        var userLocation: CLLocation?
+        if newLocation != nil {
+            userLocation = newLocation
+        } else {
+            userLocation = location
+        }
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegion(center: (userLocation?.coordinate)!, span: span)
+        mapView.setRegion(region, animated: true)
     }
     
     func searchLocationAction(sender: UIBarButtonItem) {
@@ -79,66 +89,12 @@ class MapVC: UIViewController {
             geocoder.geocodeAddressString(cityName, completionHandler: { (placemarks, error) -> Void in
                 if (placemarks?[0]) != nil {
                     let placemark = placemarks?.first!
-                    let location = placemark?.location
-                    self.newLocation = CLLocation(latitude: (location?.coordinate.latitude)!, longitude: (location?.coordinate.longitude)!)
+                    self.newLocation = placemark?.location
+                    self.goToLocationOnMap(location: self.newLocation!)
+
                 }
             })
         }
     }
     
-    private func processResponse(withPlacemarks placemarks: [CLPlacemark]?, error: Error?) {
-        
-        if let error = error {
-            print("Unable to Forward Geocode Address (\(error))")
-            print("Unable to Find Location for Address")
-            
-        } else {
-            var location: CLLocation?
-            
-            if let placemarks = placemarks, placemarks.count > 0 {
-                location = placemarks.first?.location
-            }
-            
-            if let location = location {
-                let coordinate = location.coordinate
-                print("\(coordinate.latitude), \(coordinate.longitude)")
-            } else {
-                print("No Matching Location Found")
-            }
-        }
-    }
-    
-    func getWeatherData(urlString: String) {
-        let url = URL(string: urlString)
-        
-        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
-            DispatchQueue.main.async(execute: {
-                self.setLabel(weatherData: data!)
-            })
-        }
-        
-        task.resume()
-        
-    }
-    
-    func setLabel(weatherData: Data) {
-        do {
-            let json = try JSONSerialization.jsonObject(with: weatherData, options: []) as! Dictionary<String, AnyObject>
-            
-            if let main = json["main"] as? Dictionary<String, AnyObject> {
-                if let temp = main["temp"] as? Double {
-                    print(String(format: "%.1f", temp))
-                }
-            }
-        } catch {
-            print("Error fetching data")
-        }
-    }
-
-    // MARK: - Navigation
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
-	}
-
 }
