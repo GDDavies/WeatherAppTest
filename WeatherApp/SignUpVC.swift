806ea212324eff4ab470f4d7e0667b85ebe94012
George <gdaviesdev@gmail.com>
2017-08-30 11:28:12 +0100

    Added login and signup VCs

---
+++ WeatherApp/SignUpVC.swift
@@ -0,0 +1,43 @@
+//
+//  SignUpVC.swift
+//  WeatherApp
+//
+//  Created by George Davies on 30/08/2017.
+//  Copyright © 2017 George Davies. All rights reserved.
+//
+
+import UIKit
+
+class SignUpVC: UIViewController {
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBAction func didTapSignUp(sender: UIButton) {
+        self.performSegue(withIdentifier: "ShowWeatherFromSignUp", sender: self)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
